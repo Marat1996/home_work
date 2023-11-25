f82ec897fbebd627a4bcabb2c982e9cd15e59798
Marat1996 <140758677+Marat1996@users.noreply.github.com>
2023-11-25 20:42:59 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def to_uppercase(input_str):
+    """
+    Converts all characters in the input string to uppercase.
+
+    :param input_str: The input string
+    :return: The string with all characters converted to uppercase
+    """
     return input_str.upper()
