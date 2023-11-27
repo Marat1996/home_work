ebfd8b4ca406d08432d55afd92839fbb0ea37976
Marat1996 <140758677+Marat1996@users.noreply.github.com>
2023-11-27 17:55:05 +0300

    Feature/task2 (#2)
    * Добавлена функция
    
    * Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,13 @@
 def to_uppercase(input_str):
     """
+ feature/Task2
+    Converts all characters in the input string to uppercase.
+
+    :param input_str: The input string
+    :return: The string with all characters converted to uppercase
+    """
+    return input_str.upper()
+
     Преобразует все символы строки в верхний регистр.
 
     :param input_str: Исходная строка
@@ -20,3 +28,4 @@ def capitalize_first_letters(input_str):
     words = input_str.split()
     capitalized_words = [word.capitalize() for word in words]
     return ' '.join(capitalized_words)
+ develop
